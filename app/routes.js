3a5119b317a7a2fb9459e6be99063a24e634cedf
Edward Horsford <ed.horsford@digital.cabinet-office.gov.uk>
2015-11-24 14:57:04 +0000

    Add first set of pages for webchat

--- app/routes.js
+++ app/routes.js
@@ -11,4 +11,17 @@ router.get('/examples/template-data', function (req, res) {
 
 // add your routes here
 
+router.post('/webchat/triage', function (req, res) {
+	var name = req.body.name,
+		userQuery = req.body.userQuery;
+	req.session.name = name;
+	req.session.userQuery = userQuery;
+	res.redirect('queue');
+	
+});
+
+router.get('/webchat/queue', function (req, res) {
+  res.render('webchat/queue', { 'name' : req.session.name });
+});
+
 module.exports = router;
